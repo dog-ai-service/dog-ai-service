a0079e99f1e225bd56f0f439f328b0e25a1b557e
wanter9091 <dhqudwo2976@naver.com>
2025-05-18 07:19:53 +0900

    캘린더 넣는데 오류 발생해서 점검시작

--- pages/calendar_page.py
+++ pages/calendar_page.py
@@ -1,6 +1,8 @@
 # ui
 import streamlit as st
 from services.calendar_api import calendar_api
+from components.st_calendar import st_calendar
 
 def calendar_page():
+    st_calendar()
     calendar_api()
