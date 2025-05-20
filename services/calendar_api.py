f0953d7f0ac813a799f13a9fcb953d3f6192b9b2
wanter9091 <dhqudwo2976@naver.com>
2025-05-20 16:16:41 +0900

    Test : 라디오 버튼 개조 중

--- services/calendar_api.py
+++ services/calendar_api.py
@@ -33,27 +33,34 @@ def calendar_api():
         )
         # 캘린더 API 서비스 객체 생성
         service = build("calendar", "v3", credentials=creds)
-        # 캘린더 목록 전부 가져오기
-        calendarId_select={}
+        # calendar_list_dict에 캘린더 목록 전부 가져오기
+        calendar_list_dict={}
         page_token = None
         while True:
             calendar_list = service.calendarList().list(pageToken=page_token).execute()
             for calendar_list_entry in calendar_list['items']:
-                calendarId_select[calendar_list_entry['summary']]=calendar_list_entry
+                # 캘린더 정보 아이디로 딕에 저장
+                calendar_list_dict[calendar_list_entry.get('id', '없으면 말이 안되는데...')]=calendar_list_entry
                 st.info(calendar_list_entry)
             page_token = calendar_list.get('nextPageToken')
             if not page_token:
                 break
-        
+        calendar_entries = list(calendar_list_dict.values())
+        choice = st.radio(
+            "캘린더 선택",
+            options=calendar_entries,
+            format_func=lambda entry: entry.get("summary", "이름이 없으면 말이 안되는데...")
+        )
+
         #테스트
-        st.info(calendarId_select)
+        st.info(choice.get("id","제목없음"))
         # TIME_MIN년부터 가져오기
         time_min = TIME_MIN
         # TIME_MAX년부터 가져오기
         time_max = TIME_MAX
         # 캘린더에서 대충 최신 이벤트 MAX_RESULTS개 가져오기
         events_result = service.events().list(
-            calendarId="primary",
+            calendarId=choice.get("id", '없으면 말이 안되는데...'),
             timeMin=time_min,
             timeMax=time_max,
             maxResults=MAX_RESULTS,
@@ -62,7 +69,7 @@ def calendar_api():
         ).execute()
         events = events_result.get("items", [])
         #테스트
-        st.info(f"calendar_api events : {events}")
+        #st.info(f"calendar_api events : {events}")
 
         calendar_events=[]
 
