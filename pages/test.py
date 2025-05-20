96a49109963352efa3884acbe34a444a6b8301a1
wanter9091 <dhqudwo2976@naver.com>
2025-05-20 10:49:10 +0900

    테스트 코드 추가3

--- pages/test.py
+++ pages/test.py
@@ -4,46 +4,35 @@ import streamlit as st
 from components.sidebar import sidebar
 #
 from services.drive_api import sheet_create, sheet_write, sheet_read
+# 구글 권한 사용을 위한 패키지
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+# 환경변수
+from env_config import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
 
 sidebar()
-sheet=None
-if st.button("생성버튼"):
-    st.write("생성중")
-    st.session_state.sheet=sheet_create()
 
-dogs = [
-    {
-        "이름": "뽀삐",
-        "나이": "3",
-        "몸무게": "5.2",
-        "견종": "말티즈",
-        "성별": "암컷",
-        "예방접종": "O",
-        "중성화": "X",
-        "특이사항": "알레르기 있음"
-    },
-    {
-        "이름": "초코",
-        "나이": "5",
-        "몸무게": "8.1",
-        "견종": "시바견",
-        "성별": "수컷",
-        "예방접종": "X",
-        "중성화": "O",
-        "특이사항": "사람을 좋아함"
-    }
-]
+client_id=GOOGLE_CLIENT_ID
+client_secret=GOOGLE_CLIENT_SECRET
 
-if st.button("정보추가"):
-    st.write(sheet)
-    if "sheet" not in st.session_state:
-        st.write("생성 버튼을 먼저 누르세요")
-    else:
-        st.write("정보추가중")
-        sheet_write(
-            spreadsheet_id=st.session_state.get("sheet","미기입"),
-            dogs=dogs,
-            )
-        sheet_read(spreadsheet_id=st.session_state.get("sheet","미기입"))
-        
+if "token" in st.session_state:
+    # 캘린더 목록 가져오기
+    token = st.session_state.token
+        # 캘린더에 사용을 위한 구글계정 정보를 세션에서 가져오기
+    creds = Credentials(
+        token=token["token"]["access_token"],
+        refresh_token=token.get("refresh_token"),
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=client_id,
+        client_secret=client_secret,
+        scopes=["https://www.googleapis.com/auth/calendar"]
+    )
+    # 캘린더 API 서비스 객체 생성
+    service = build("calendar", "v3", credentials=creds)
+    calendar_list = service.calendarList().list().execute()
 
+    # 각 캘린더 출력
+    for calendar_entry in calendar_list.get("items", []):
+        st.info(f"캘린더 이름:{calendar_entry.get("summary","없음")}")
+        st.info(f"캘린더 ID: {calendar_entry.get("id","없음")}")
+        st.info(f"소유권:{calendar_entry.get("accessRole","없음")}")
